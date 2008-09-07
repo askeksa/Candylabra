fe6d30b1d28e929732876e63c47c4fcb9f9432f1
Aske Simon Christensen <askeksa@users.noreply.github.com>
2008-09-07 19:19:46 +0000

    More intro-specific separation

--- RenderDLL/main.h
+++ RenderDLL/main.h
@@ -1,4 +1,34 @@
 #include <d3dx9.h>
+#include <dxerr.h>
+#include <cstdio>
+
+#if 1
+static void check(char *n, int r) {
+	if (r != D3D_OK) {
+		char buff[512];
+		sprintf(buff, "%s returned %s\n", n, DXGetErrorString(r));
+		MessageBox(0, buff, 0, 0);
+		ExitProcess(1);
+	}
+}
+#define CHECK(s) check(#s, s)
+
+static ID3DXBuffer *errors;
+static void checkcompile(char *statement, int error) {
+	if (error != D3D_OK) {
+		char buff[2048];
+		sprintf(buff, "%s\nfailed with error %s\n%s", statement, DXGetErrorString(error), errors->GetBufferPointer());
+		MessageBox(0, buff, 0, 0);
+		exit(1);
+	}
+}
+#define CHECKC(s) checkcompile(#s, s)
+#define ERRORS &errors
+#else
+#define CHECK(s) (s)
+#define CHECKC(s) (s)
+#define ERRORS NULL
+#endif
 
 extern "C" {
 	void uploadparams();
--- RenderDLL/render.cpp
+++ RenderDLL/render.cpp
@@ -2,18 +2,23 @@
 #include "main.h"
 #include "comcall.h"
 
+void render_init()
+{
+	CHECK(D3DXCreateBox(COMHandles.device, 1.0f, 1.0f, 1.0f, &COMHandles.boxmesh, NULL));
+}
+
+void render_main()
+{
+	view_display();
+
+	COMHandles.effect->Begin(0, 0);
+	COMHandles.device->Clear(0, NULL, D3DCLEAR_ZBUFFER|D3DCLEAR_TARGET, 0, 1.0f, 0);
+	COMHandles.effect->SetMatrixTranspose("vp", &proj);
+	pass(0,0);
+	COMHandles.effect->End();
+}
+
 extern "C" {
-
-	void __stdcall render()
-	{
-		view_display();
-
-		COMHandles.effect->Begin(0, 0);
-		COMHandles.device->Clear(0, NULL, D3DCLEAR_ZBUFFER|D3DCLEAR_TARGET, 0, 1.0f, 0);
-		COMHandles.effect->SetMatrixTranspose("vp", &proj);
-		pass(0,0);
-		COMHandles.effect->End();
-	}
 
 	void __stdcall drawprimitive(float r, float g, float b, float a, int ptype)
 	{
